b2237a0e56be76c7e3c9420babb268adca5b0e99
Alexander <alexanderholmberg91@gmail.com>
2022-10-11 16:33:08 +0200

    generation flow

--- frontend/pages/create.tsx
+++ frontend/pages/create.tsx
@@ -6,6 +6,14 @@ import { FiExternalLink } from 'react-icons/fi';
 import { MdAddCircle } from 'react-icons/md';
 import Select from 'react-select';
 
+const valueToCollection: { [key: string]: string } = {
+  random: 'The Random Collection',
+  space: 'The Space Collection',
+  walterwhite: 'The Walter White Collection',
+  pepes: 'The Pepe Collection',
+  trash: 'The Trash Collection',
+};
+
 export const modelOptions = [
   {
     value: 'stable-diffusion',
@@ -39,7 +47,7 @@ export const collectionOptions = [
     label: (
       <div className="flex gap-2 items-center p-3">
         <TbWorld />
-        <span>Random</span>
+        <span>The Random Collection</span>
       </div>
     ),
   },
@@ -48,7 +56,7 @@ export const collectionOptions = [
     label: (
       <div className="flex gap-2 items-center p-3">
         <FaRocket />
-        <span>Space</span>
+        <span>The Space Collection</span>
       </div>
     ),
   },
@@ -57,7 +65,7 @@ export const collectionOptions = [
     label: (
       <div className="flex gap-2 items-center p-3">
         <FaFrog />
-        <span>Pepe&apos;s</span>
+        <span>The Pepe Collection</span>
       </div>
     ),
   },
@@ -66,7 +74,7 @@ export const collectionOptions = [
     label: (
       <div className="flex gap-2 items-center p-3">
         <FaHatCowboy />
-        <span>Walter White</span>
+        <span>The Walter White Collection</span>
       </div>
     ),
   },
@@ -75,7 +83,7 @@ export const collectionOptions = [
     label: (
       <div className="flex gap-2 items-center p-3">
         <FaTrashAlt />
-        <span>Trash</span>
+        <span>The Trash Collection</span>
       </div>
     ),
   },
@@ -153,11 +161,10 @@ export default function Create() {
   const [model, setModel] = useState('stable-diffusion');
   const { register, handleSubmit, watch, formState } = useForm();
   const [progress, setProgress] = useState(0);
-  const [generatedImages, setGeneratedImages] = useState<string[]>([]);
-  const [chosenImage, setChosenImage] = useState(0);
+  const [generatedImage, setGeneratedImage] = useState('');
 
   const onSubmit = (prompt: any) => {
-    setGeneratedImages([]);
+    setGeneratedImage('');
     console.log(prompt);
 
     const timer = setInterval(() => {
@@ -166,18 +173,16 @@ export default function Create() {
           clearInterval(timer);
           return 0;
         }
-        return oldProgress + 10;
+        return oldProgress + 1;
         // const diff = Math.random() * 10;
         // return Math.min(oldProgress + diff, 100);
       });
     }, 50);
 
-    setGeneratedImages([
-      'space1.png',
-      'space2.png',
-      'space3.png',
-      'space4.png',
-    ]);
+    // setTimeout is to prevent a UI bug
+    setTimeout(() => {
+      setGeneratedImage('space1.png');
+    }, 50);
   };
 
   const handleChangeCollection = (selectedOption: any) => {
@@ -189,7 +194,7 @@ export default function Create() {
   };
 
   return (
-    <div className="bg-black text-white h-screen flex flex-col gap-4 items-center">
+    <div className="bg-black text-white flex flex-col gap-4 items-center pb-8">
       <div className="w-3/4 lg:w-[48rem] flex flex-col gap-4">
         <label className="text-xl">Choose collection</label>
         <Select
@@ -218,7 +223,7 @@ export default function Create() {
       <div className="w-3/4 lg:w-[48rem] flex flex-col gap-4">
         <div className="flex flex-col gap-2 md:gap-4 items-center mb-3">
           <h1 className="text-2xl md:text-3xl text-center">
-            The Random Collection
+            {valueToCollection[collection]}
           </h1>
           <p className="text-center w-full md:w-5/6 text-sm md:text-base">
             Stable Diffusion is a state of the art text-to-image model that
@@ -241,16 +246,89 @@ export default function Create() {
         />
         <form onSubmit={handleSubmit(onSubmit)}>
           <input
-            className="bg-zinc-800 lg:text-lg font-semibold px-4 py-3 w-4/6 md:w-4/6 lg:w-4/5 rounded-l-lg outline-none mb-2"
+            className="bg-zinc-800 lg:text-lg px-4 py-3 w-4/6 md:w-4/6 lg:w-4/5 rounded-l-md outline-none mb-2"
+            placeholder="Enter your prompt..."
             {...register('prompt', { required: true })}
           />
           <button
             type="submit"
-            className="bg-green-500 text-black text-base lg:text-lg font-semibold px-4 py-3 w-2/6 md:w-2/6 lg:w-1/5 rounded-r-lg outline-none"
+            className="bg-green-400 text-black text-base lg:text-lg font-semibold px-4 py-3 w-2/6 md:w-2/6 lg:w-1/5 rounded-r-md outline-none hover:bg-white"
           >
             Generate
           </button>
         </form>
+        {(generatedImage === '' || progress !== 0) && (
+          <div>
+            <div className="relative mb-4 w-full h-64 bg-zinc-900 rounded-lg ">
+              <div
+                className="h-64 bg-zinc-800 rounded-lg"
+                style={{ width: Math.floor(progress) + '%' }}
+              ></div>
+              <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-lg">
+                {progress === 0
+                  ? 'Ready to generate images!'
+                  : `${Math.floor(progress)}%`}
+              </p>
+            </div>
+          </div>
+        )}
+        {generatedImage !== '' && progress === 0 && (
+          <div className="grid grid-cols-2 gap-12 mt-3">
+            <div className="flex flex-col gap-4">
+              <img
+                src={`/generated/${generatedImage}`}
+                alt="generated image"
+                className=" rounded-lg"
+              ></img>
+              <div className="grid grid-cols-2 gap-4 font-semibold">
+                <div className="flex flex-col gap-2 bg-zinc-800 rounded-md p-4">
+                  <p className="text-sm">Model</p>
+                  <p>Stable Diffusion</p>
+                </div>
+                <div className="flex flex-col gap-2 bg-zinc-800 rounded-md p-4">
+                  <p className="text-sm">Creator</p>
+                  <p>0xdeadbeef...1337</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl font-semibold mt-5">Space Image #324</h1>
+              <p className="text-lg">Something cool in space, many colors</p>
+              <p className="text-xl font-semibold mt-2">Collection</p>
+              <div className="flex items-center gap-5">
+                <img
+                  src={`/generated/space2.png`}
+                  alt="generated image"
+                  className="h-12 w-12 rounded-md"
+                ></img>
+                <p>{valueToCollection[collection]}</p>
+              </div>
+              <p className="font-semibold text-xl mt-3">Details</p>
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center justify-between">
+                  <p className="font-semibold text-lg">Royalties</p>
+                  <p className="text-lg text-gray-400">
+                    5% goes to the creator
+                  </p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <p className="font-semibold text-lg">Minted</p>
+                  <p className="text-lg text-gray-400">Feb 6, 2022</p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <p className="font-semibold text-lg">Owned by</p>
+                  <p className="text-lg text-gray-400">0xdeadbeef...1337</p>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="mt-4 bg-green-400 text-black font-semibold px-4 py-3 rounded-md outline-none hover:bg-white"
+              >
+                Mint NFT
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
